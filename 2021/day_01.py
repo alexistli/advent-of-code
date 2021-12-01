a653bf539d57155bfc309661ea1c85450d2d6f47
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-01 10:30:34 +0100

    Solution for day_01_part_2

--- 2021/day_01.py
+++ 2021/day_01.py
@@ -4,3 +4,6 @@ with open("inputs/day_01.txt", "r") as text_file:
 result = sum(1 for i in range(1, len(data)) if data[i] > data[i - 1])
 print(result)
 
+windows = [sum(data[i-2:i+1]) for i in range(2, len(data))]
+result = sum(1 for idx, window in enumerate(windows[1:]) if window > windows[idx])
+print(result)
