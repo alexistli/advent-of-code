1482fc034739bcc62ede68ea1ef6ac52e649e835
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-02 10:15:09 +0100

    Solution for day_02_part_1

---
+++ 2021/day_02.py
@@ -0,0 +1,17 @@
+with open("inputs/day_02.txt", "r") as text_file:
+    lines = text_file.readlines()
+    for line in lines:
+        print(line.split())
+    data = [(line.split()[0], int(line.split()[1])) for line in lines]
+
+horizontal_pos = 0
+depth = 0
+for line in data:
+    if line[0] == "forward":
+        horizontal_pos += line[1]
+    elif line[0] == "up":
+        depth -= line[1]
+    elif line[0] == "down":
+        depth += line[1]
+
+print(horizontal_pos, depth, horizontal_pos*depth)
