d269ee71a9149dd91060d6015695a003d18ff37d
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-03 10:23:11 +0100

    Solution for day_03_part_2

--- 2021/day_03.py
+++ 2021/day_03.py
@@ -19,3 +19,29 @@ epsilon_rate_bin = int("".join(epsilon_rate_bits), 2)
 print(f"gamma_rate_bin: {gamma_rate_bin}, epsilon_rate_bin: {epsilon_rate_bin}")
 print(f"product: {gamma_rate_bin * epsilon_rate_bin}")
 
+
+# ======== Part 2 ========
+print("\n\n")
+oxygen_candidates = list(data)
+counter = 0
+while len(oxygen_candidates) > 1:
+    number_bits = sum(int(bit) for col in oxygen_candidates for bit in col[counter] if bit == "1")
+    threshold = (len(oxygen_candidates) + 1) // 2
+    oxygen_reference = "1" if number_bits >= threshold else "0"
+    oxygen_candidates = [candidate for candidate in oxygen_candidates if candidate[counter] == oxygen_reference]
+    counter += 1
+
+co2_candidates = list(data)
+counter = 0
+while len(co2_candidates) > 1:
+    number_bits = sum(int(bit) for col in co2_candidates for bit in col[counter] if bit == "1")
+    threshold = (len(co2_candidates) + 1) // 2
+    co2_reference = "0" if number_bits >= threshold else "1"
+    co2_candidates = [candidate for candidate in co2_candidates if candidate[counter] == co2_reference]
+    counter += 1
+
+
+oxygen_rate_bin = int("".join(oxygen_candidates[0]), 2)
+co2_rate_bin = int("".join(co2_candidates[0]), 2)
+print(f"oxygen_rate_bin: {oxygen_rate_bin}, co2_rate_bin: {co2_rate_bin}")
+print(f"product: {oxygen_rate_bin * co2_rate_bin}")
