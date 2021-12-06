92f65e7e9adb3c29fa96f3160625ddaa5e1a74ef
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-06 10:43:47 +0100

    Solution for day_06 part 1 working with test input

---
+++ 2021/day_06.py
@@ -0,0 +1,21 @@
+with open("inputs/day_06.txt", "r") as text_file:
+    line = text_file.readline()
+    data = [int(age) for age in line.strip().split(",")]
+
+print(data)
+
+fishes = list(data)
+counter = 80
+while counter > 0:
+    fishlings = []
+    for fish_idx, fish_age in enumerate(fishes):
+        fish_age -= 1
+        if fish_age < 0:
+            fish_age = 6
+            fishlings.append(8)
+        fishes[fish_idx] = fish_age
+    counter -= 1
+    fishes.extend(fishlings)
+
+print(len(fishes))
+print(fishes)
