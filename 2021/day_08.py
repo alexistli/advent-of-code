1e15f91f69ba1bd7fc627ac14cd098bb3a8fbfd5
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-08 09:21:53 +0100

    Add solution working for test input

--- 2021/day_08.py
+++ 2021/day_08.py
@@ -7,8 +7,26 @@ with open("inputs/day_08.txt", "r") as text_file:
         output = output.split()
         data.append([patterns, output])
 
-    for line in data:
-        print(line)
+for line in data:
+    print(line)
+
+ones = 0
+fours = 0
+sevens = 0
+heights = 0
+for line in data:
+    for digit in line[1]:
+        if len(digit) == 2:
+            ones += 1
+        elif len(digit) == 4:
+            fours += 1
+        elif len(digit) == 3:
+            sevens += 1
+        elif len(digit) == 7:
+            heights += 1
+
+occurences = ones + fours + sevens + heights
+print("Solution for part 1:", occurences)
 
 # 0:
 # 1:
