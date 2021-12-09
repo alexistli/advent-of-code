57659994f5e4416341e5f5a9047e58e5816c9b1f
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-09 13:50:54 +0100

    Update solution for day_09 part 1

--- 2021/day_09.py
+++ 2021/day_09.py
@@ -1,10 +1,10 @@
 with open('inputs/day_09.txt', mode='r') as f:
     lines = f.readlines()
     data = [[int(point) for point in list(line.strip())] for line in lines]
-    print(data)
+
 
 # ================ Part 1 ================
-low_points = []
+low_points = {}
 for i, row in enumerate(data):
     for j, point in enumerate(row):
         adjacent_up = data[i - 1][j] if i - 1 >= 0 else float('+inf')
@@ -12,6 +12,7 @@ for i, row in enumerate(data):
         adjacent_left = data[i][j - 1] if j - 1 >= 0 else float('+inf')
         adjacent_right = data[i][j + 1] if j + 1 <= len(data[0]) - 1 else float('+inf')
         if point < min(adjacent_up, adjacent_down, adjacent_left, adjacent_right):
-            low_points.append(point)
-total_risk_level = sum(point + 1 for point in low_points)
+            low_points[(i, j)] = point
+total_risk_level = sum(point + 1 for point in low_points.values())
 print("Solution for part 1:", total_risk_level)
+
