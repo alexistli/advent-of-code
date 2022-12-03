9584246939002bdce7a6e3c5bbec1c0aca039d81
Alexis Torelli <alexis.torelli@napta.io>
2022-12-03 17:06:53 +0100

    extract function to reuse for part_2

--- 2022/day_01/part_1.py
+++ 2022/day_01/part_1.py
@@ -1,5 +1,10 @@
-def get_calories_by_elf(calories_list):
+def load_day_input():
+    with open("2022/day_01/input.txt", "r") as f:
+        data = f.readlines()
+    return data
 
+
+def get_calories_by_elf(calories_list):
     elf_calories = []
     for calories in calories_list:
         if calories == "\n":
@@ -10,9 +15,7 @@ def get_calories_by_elf(calories_list):
 
 
 def main():
-    with open("2022/day_01/input.txt", "r") as f:
-        data = [line for line in f.readlines()]
-
+    data = load_day_input()
     calories_grouped_by_elf = get_calories_by_elf(data)
     max_elf_colories = max(
         [sum(elf_calories) for elf_calories in calories_grouped_by_elf]
