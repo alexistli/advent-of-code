1ae4ebf186516d7b9b7a824d0808e8f8b0c89f47
Alexis Torelli <alexis.torelli@napta.io>
2022-12-03 18:36:25 +0100

    fix load_day_input in helpers

--- aoc_2022/helpers.py
+++ aoc_2022/helpers.py
@@ -1,4 +1,4 @@
-def load_day_input(path):
-    with open("aoc_2022/day_01/input.txt", "r") as f:
+def load_day_input(path: str) -> list[str]:
+    with open(path) as f:
         data = f.readlines()
     return data
