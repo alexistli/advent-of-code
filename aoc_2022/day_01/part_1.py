703e2e4b63be97164f92e106b078756606e18c68
Alexis Torelli <alexis.torelli@napta.io>
2022-12-03 18:13:01 +0100

    add template and helpers

--- aoc_2022/day_01/part_1.py
+++ aoc_2022/day_01/part_1.py
@@ -1,7 +1,4 @@
-def load_day_input():
-    with open("aoc_2022/day_01/input.txt", "r") as f:
-        data = f.readlines()
-    return data
+from aoc_2022.helpers import load_day_input
 
 
 def get_calories_by_elf(calories_list):
@@ -15,7 +12,7 @@ def get_calories_by_elf(calories_list):
 
 
 def main():
-    data = load_day_input()
+    data = load_day_input("aoc_2022/day_01/input.txt")
     calories_grouped_by_elf = get_calories_by_elf(data)
     max_elf_colories = max(
         [sum(elf_calories) for elf_calories in calories_grouped_by_elf]
---
+++ aoc_2022/helpers.py
@@ -0,0 +1,4 @@
+def load_day_input(path):
+    with open("aoc_2022/day_01/input.txt", "r") as f:
+        data = f.readlines()
+    return data
