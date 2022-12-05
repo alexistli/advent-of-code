7dbe9a8dde1d20d29415b73552b4763a5ca70fa8
Alexis Torelli <alexis.torelli@napta.io>
2022-12-05 07:52:55 +0100

    add boilerplate code for day_05

---
+++ aoc_2022/day_05/part_2.py
@@ -0,0 +1,12 @@
+from aoc_2022.helpers import load_day_input
+
+from pathlib import Path
+
+
+def main():
+    data = load_day_input(Path(__file__).parent / "input.txt")
+    return data
+
+
+if __name__ == "__main__":
+    main()
