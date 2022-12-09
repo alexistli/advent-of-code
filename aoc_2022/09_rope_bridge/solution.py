df5821a480526fa10bad88022adb437b5783f3a6
Alexis Torelli <alexis.torelli@napta.io>
2022-12-09 10:00:01 +0100

    add boilerplate code for day 08

---
+++ aoc_2022/09_rope_bridge/solution.py
@@ -0,0 +1,35 @@
+"""AoC 9, 2022: Rope Bridge."""
+
+
+from aoc_2022.helpers import load_day_input
+
+import pathlib
+
+
+def parse_data(puzzle_input):
+    """Parse input."""
+
+
+def part1(data):
+    """Solve part 1."""
+
+
+def part2(data):
+    """Solve part 2."""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse_data(puzzle_input)
+    yield part1(data)
+    yield part2(data)
+
+
+if __name__ == "__main__":
+    examples = solve(load_day_input(pathlib.Path(__file__).parent / "example.txt"))
+    print("Examples:\n\t{}".format("\n\t".join(str(solution) for solution in examples)))
+
+    solutions = solve(load_day_input(pathlib.Path(__file__).parent / "input.txt"))
+    print(
+        "Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions))
+    )
