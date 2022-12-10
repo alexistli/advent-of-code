af2bf2066a73ae6f739410cc2d3f2a45f0b603fc
Alexis Torelli <alexis.torelli@napta.io>
2022-12-10 15:58:18 +0100

    update template

--- aoc_2022/template/solution.py
+++ aoc_2022/template/solution.py
@@ -5,20 +5,23 @@ from aoc_2022.helpers import load_day_input
 
 import pathlib
 
+CWD = pathlib.Path(__file__).parent
 
-def parse_data(puzzle_input):
+
+def parse_data(puzzle_input: list[str]) -> list[str]:
     """Parse input."""
+    return [row.strip("\n") for row in puzzle_input]
 
 
-def part1(data):
+def part1(data: list[str]):
     """Solve part 1."""
 
 
-def part2(data):
+def part2(data: list[str]):
     """Solve part 2."""
 
 
-def solve(puzzle_input):
+def solve(puzzle_input: list[str]):
     """Solve the puzzle for the given input."""
     data = parse_data(puzzle_input)
     yield part1(data)
@@ -26,10 +29,8 @@ def solve(puzzle_input):
 
 
 if __name__ == "__main__":
-    examples = solve(load_day_input(pathlib.Path(__file__).parent / "example.txt"))
-    print("Examples:\n\t{}".format("\n\t".join(str(solution) for solution in examples)))
+    examples = solve(load_day_input(CWD / "example.txt"))
+    print("Examples:\n\t{}".format("\n\t".join(str(e) for e in examples)))
 
-    solutions = solve(load_day_input(pathlib.Path(__file__).parent / "input.txt"))
-    print(
-        "Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions))
-    )
+    solutions = solve(load_day_input(CWD / "input.txt"))
+    print("Solutions:\n\t{}".format("\n\t".join(str(s) for s in solutions)))
