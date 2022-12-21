c48aff4a158b7f03a35a9540115dcf462357731c
Alexis Torelli <alexis.torelli@napta.io>
2022-12-21 02:29:01 +0100

    rename function in helpers

--- aoc_2022/01_calorie_counting/part_1.py
+++ aoc_2022/01_calorie_counting/part_1.py
@@ -1,4 +1,4 @@
-from aoc_2022.helpers import load_day_input
+from aoc_2022.helpers import get_input
 
 
 def get_calories_by_elf(calories_list):
@@ -12,7 +12,7 @@ def get_calories_by_elf(calories_list):
 
 
 def main():
-    data = load_day_input("aoc_2022/day_01/input.txt")
+    data = get_input("aoc_2022/day_01/input.txt")
     calories_grouped_by_elf = get_calories_by_elf(data)
     max_elf_colories = max(
         [sum(elf_calories) for elf_calories in calories_grouped_by_elf]
