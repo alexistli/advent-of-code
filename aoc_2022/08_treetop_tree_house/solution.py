8af116c39cb7bef46f1c37f0a39b9d84c0dc9e07
Alexis Torelli <alexis.torelli@napta.io>
2022-12-31 11:31:13 +0100

    add utils, refactor script and add typing on day 8

--- aoc_2022/08_treetop_tree_house/solution.py
+++ aoc_2022/08_treetop_tree_house/solution.py
@@ -1,36 +1,40 @@
 """AoC 8, 2022: Treetop Tree House."""
 
-
 import math
 import pathlib
-from typing import Iterator
+from typing import Iterable, Iterator
 
-from aoc_2022.utils import get_input, parse_data
+from aoc_2022.utils import get_input, parse_data, parse_ints
 
 
-def get_row(i: int, _j: int, data: list[str]) -> str:
+Tree = int
+Matrix = list[list[int]]
+
+
+def get_row(i: int, _j: int, data: Matrix) -> list[Tree]:
     return data[i]
 
 
-def get_column(i: int, j: int, data: list[str]) -> str:
+def get_column(i: int, j: int, data: Matrix) -> list[Tree]:
     return get_row(j, i, list(zip(*data)))
 
 
-def check_tree_visibility_from_trees(tree: str, trees: list[str]) -> bool:
+def check_tree_visibility_from_trees(tree: Tree, trees: Iterable[Tree]) -> bool | None:
     if not trees:
         return True
     if max(trees) < tree:
         return True
+    return None
 
 
-def check_tree_visibility(i: int, j: int, tree: str, data: list[str]) -> bool:
+def check_tree_visibility(i: int, j: int, tree: Tree, data: Matrix) -> bool | None:
     row = get_row(i, j, data)
     left, right = row[:j], row[j + 1 :]
 
     col = get_column(i, j, data)
     upper, lower = col[:i], col[i + 1 :]
 
-    return (
+    return bool(
         check_tree_visibility_from_trees(tree, left)
         or check_tree_visibility_from_trees(tree, right)
         or check_tree_visibility_from_trees(tree, upper)
@@ -40,15 +44,16 @@ def check_tree_visibility(i: int, j: int, tree: str, data: list[str]) -> bool:
 
 def part1(data: list[str]) -> int:
     """Solve part 1."""
+    parsed_data = [parse_ints(list(row)) for row in data]
     nb_visible_trees = 0
-    for i, row in enumerate(data):
+    for i, row in enumerate(parsed_data):
         for j, tree in enumerate(row):
-            if check_tree_visibility(i, j, tree, data):
+            if check_tree_visibility(i, j, tree, parsed_data):
                 nb_visible_trees += 1
     return nb_visible_trees
 
 
-def get_distance(tree: str, trees: list[str]) -> int:
+def get_distance(tree: Tree, trees: Iterable[Tree]) -> int:
     distance = 0
     for t in trees:
         distance += 1
@@ -57,7 +62,7 @@ def get_distance(tree: str, trees: list[str]) -> int:
     return distance
 
 
-def get_scenic_score(i: int, j: int, tree: str, data: list[str]) -> int:
+def get_scenic_score(i: int, j: int, tree: Tree, data: Matrix) -> int:
     row = get_row(i, j, data)
     left, right = row[:j], row[j + 1 :]
 
@@ -76,12 +81,11 @@ def get_scenic_score(i: int, j: int, tree: str, data: list[str]) -> int:
 
 def part2(data: list[str]) -> int:
     """Solve part 2."""
+    parsed_data = [parse_ints(list(row)) for row in data]
     best_scenic_score = 0
-    for i, row in enumerate(data):
+    for i, row in enumerate(parsed_data):
         for j, tree in enumerate(row):
-            best_scenic_score = max(
-                best_scenic_score, get_scenic_score(i, j, tree, data)
-            )
+            best_scenic_score = max(best_scenic_score, get_scenic_score(i, j, tree, parsed_data))
     return best_scenic_score
 
 
@@ -97,6 +101,4 @@ if __name__ == "__main__":
     print("Examples:\n\t{}".format("\n\t".join(str(solution) for solution in examples)))
 
     solutions = solve(get_input(pathlib.Path(__file__).parent / "input.txt"))
-    print(
-        "Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions))
-    )
+    print("Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions)))
--- aoc_2022/utils.py
+++ aoc_2022/utils.py
@@ -13,3 +13,10 @@ def get_input(path: pathlib.Path) -> list[str]:
 def parse_data(puzzle_input: list[str]) -> list[str]:
     """Parse input."""
     return [row.strip("\n") for row in puzzle_input]
+
+
+def parse_ints(input: list[str]) -> list[int]:
+    """Parse integers in a list/tuple of strings."""
+    if not isinstance(input, (list, tuple)):
+        raise TypeError("Unexpected type for input: %s. Type must be string or tuple.", type(input))
+    return [int(s) for s in input]
