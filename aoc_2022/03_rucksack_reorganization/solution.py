509a168af306c2f850a7ab3f8ce0c6651c31c970
Alexis Torelli <alexis.torelli@napta.io>
2023-01-02 12:13:33 +0100

    apply black formatting and minor function update

--- aoc_2022/03_rucksack_reorganization/solution.py
+++ aoc_2022/03_rucksack_reorganization/solution.py
@@ -1,6 +1,5 @@
 """AoC 3, 2022: Rucksack Reorganization."""
 
-
 from collections import Counter
 import pathlib
 import string
@@ -8,22 +7,17 @@ from typing import Iterator
 
 from aoc_2022.utils import get_input, parse_data
 
-PRIORITY_MAPPING = {
-    l: idx + 1 for idx, l in enumerate(string.ascii_lowercase + string.ascii_uppercase)
-}
+
+PRIORITY_MAPPING = {l: idx + 1 for idx, l in enumerate(string.ascii_lowercase + string.ascii_uppercase)}
 
 
-def split_compartments(items: str) -> list[str]:
+def split_compartments(items: str) -> tuple[str, str]:
     nb_items = len(items)
     return items[: nb_items // 2], items[nb_items // 2 :]
 
 
-def get_items_both_compartments(
-    compartment_1_items: str, compartment_2_items: str
-) -> str:
-    for item in compartment_2_items:
-        if item in compartment_1_items:
-            return item
+def get_items_both_compartments(compartment_1_items: str, compartment_2_items: str) -> str:
+    return next(item for item in compartment_2_items if item in compartment_1_items)
 
 
 def compute_item_priority(item: str) -> int:
@@ -35,9 +29,7 @@ def part1(data: list[str]) -> int:
     duplicate_compartment_items = []
     for row in data:
         compartment_1_items, compartment_2_items = split_compartments(row)
-        items_both_compartments = get_items_both_compartments(
-            compartment_1_items, compartment_2_items
-        )
+        items_both_compartments = get_items_both_compartments(compartment_1_items, compartment_2_items)
         duplicate_compartment_items.append(items_both_compartments)
 
     priority_sum = 0
@@ -56,10 +48,8 @@ def get_item_common_among_triplet(elf_triplet: list[str]) -> str:
     rucksack_1 = set(elf_triplet[0])
     rucksack_2 = set(elf_triplet[1])
     rucksack_3 = set(elf_triplet[2])
-    item_occurence_by_elf = Counter(
-        list(rucksack_1) + list(rucksack_2) + list(rucksack_3)
-    )
-    return item_occurence_by_elf.most_common(1)[0][0]
+    item_occurrence_by_elf = Counter(list(rucksack_1) + list(rucksack_2) + list(rucksack_3))
+    return item_occurrence_by_elf.most_common(1)[0][0]
 
 
 def part2(data: list[str]) -> int:
@@ -85,6 +75,4 @@ if __name__ == "__main__":
     print("Examples:\n\t{}".format("\n\t".join(str(solution) for solution in examples)))
 
     solutions = solve(get_input(pathlib.Path(__file__).parent / "input.txt"))
-    print(
-        "Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions))
-    )
+    print("Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions)))
