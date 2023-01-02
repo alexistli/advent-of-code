9b23e7524486f3b3a91b6e785d05b51ed1820f47
Alexis Torelli <alexis.torelli@napta.io>
2023-01-02 12:18:26 +0100

    move constant in inner scope

--- aoc_2022/template/solution.py
+++ aoc_2022/template/solution.py
@@ -1,12 +1,9 @@
 """AoC {day}, {year}: {puzzle_name}."""
 
-
 import pathlib
 from typing import Any, Iterator
 
 from aoc_2022.utils import get_input, parse_data
-
-CWD = pathlib.Path(__file__).parent
 
 
 def part1(data: list[str]) -> Any:
@@ -25,8 +22,10 @@ def solve(puzzle_input: list[str]) -> Iterator[Any]:
 
 
 if __name__ == "__main__":
-    examples = solve(get_input(CWD / "example.txt"))
+    cwd = pathlib.Path(__file__).parent
+
+    examples = solve(get_input(cwd / "example.txt"))
     print("Examples:\n\t{}".format("\n\t".join(str(e) for e in examples)))
 
-    solutions = solve(get_input(CWD / "input.txt"))
+    solutions = solve(get_input(cwd / "input.txt"))
     print("Solutions:\n\t{}".format("\n\t".join(str(s) for s in solutions)))
