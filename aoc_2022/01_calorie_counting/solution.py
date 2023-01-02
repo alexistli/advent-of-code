489b4bfa90c0c326dd8cbdf28e5282eaf32a3633
Alexis Torelli <alexis.torelli@napta.io>
2023-01-02 12:11:28 +0100

    apply black formatting

--- aoc_2022/01_calorie_counting/solution.py
+++ aoc_2022/01_calorie_counting/solution.py
@@ -1,6 +1,5 @@
 """AoC 1, 2022: Calorie Counting."""
 
-
 import pathlib
 from typing import Iterator
 
@@ -8,7 +7,7 @@ from aoc_2022.utils import get_input, parse_data
 
 
 def get_calories_by_elf(calories_list: list[str]) -> Iterator[list[int]]:
-    elf_calories = []
+    elf_calories: list[int] = []
     for calories in calories_list:
         if calories == "":
             yield elf_calories
@@ -20,19 +19,15 @@ def get_calories_by_elf(calories_list: list[str]) -> Iterator[list[int]]:
 def part1(data: list[str]) -> int:
     """Solve part 1."""
     calories_grouped_by_elf = get_calories_by_elf(data)
-    max_elf_colories = max(
-        [sum(elf_calories) for elf_calories in calories_grouped_by_elf]
-    )
-    return max_elf_colories
+    max_elf_calories = max([sum(elf_calories) for elf_calories in calories_grouped_by_elf])
+    return max_elf_calories
 
 
 def part2(data: list[str]) -> int:
     """Solve part 2."""
     calories_grouped_by_elf = get_calories_by_elf(data)
-    summed_elf_colories = [
-        sum(elf_calories) for elf_calories in calories_grouped_by_elf
-    ]
-    top_3_elf_calories = sorted(summed_elf_colories, reverse=True)[:3]
+    summed_elf_calories = [sum(elf_calories) for elf_calories in calories_grouped_by_elf]
+    top_3_elf_calories = sorted(summed_elf_calories, reverse=True)[:3]
     sum_top_3_elf_calories = sum(top_3_elf_calories)
     return sum_top_3_elf_calories
 
@@ -49,6 +44,4 @@ if __name__ == "__main__":
     print("Examples:\n\t{}".format("\n\t".join(str(solution) for solution in examples)))
 
     solutions = solve(get_input(pathlib.Path(__file__).parent / "input.txt"))
-    print(
-        "Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions))
-    )
+    print("Solutions:\n\t{}".format("\n\t".join(str(solution) for solution in solutions)))
